2d0926c551dc1cc4b374ced9d57712d74ff37932
Nick Brouwer <44852318+nebrouwer@users.noreply.github.com>
2019-06-08 15:35:21 -0700

    Added Open and Replace features

--- src/SimpleNotePad.java
+++ src/SimpleNotePad.java
@@ -2,30 +2,21 @@
 // CS 410
 // Assignment 3
 
-import java.awt.Dimension;
-import java.awt.Graphics;
+import javax.swing.*;
+import javax.swing.text.Position;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
-import javax.swing.text.Position;
-import javax.swing.text.StyledDocument;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class SimpleNotePad extends JFrame implements ActionListener{
+public class SimpleNotePad extends JFrame{
     JMenuBar menu = new JMenuBar();
     JMenu fileMenu = new JMenu("File");
     JMenu editMenu = new JMenu("Edit");
@@ -33,32 +24,44 @@ public class SimpleNotePad extends JFrame implements ActionListener{
     JMenuItem newFile = new JMenuItem("New File");
     JMenuItem saveFile = new JMenuItem("Save File");
     JMenuItem printFile = new JMenuItem("Print File");
-    JMenuItem undo = new JMenuItem("Undo");
+    JMenuItem openFile = new JMenuItem("Open File");
+    JMenuItem recentFile = new JMenuItem("Recent");
+    JMenuItem replace = new JMenuItem("Replace");
     JMenuItem copy = new JMenuItem("Copy");
     JMenuItem paste = new JMenuItem("Paste");
+    ArrayList<File> recents = new ArrayList<>();
 
-    public SimpleNotePad() {
+    public SimpleNotePad()
+    {
         setTitle("A Simple Notepad Tool");
         fileMenu.add(newFile);
+        fileMenu.addSeparator();
+        fileMenu.add(openFile);
         fileMenu.addSeparator();
         fileMenu.add(saveFile);
         fileMenu.addSeparator();
         fileMenu.add(printFile);
-        editMenu.add(undo);
+        fileMenu.addSeparator();
+        fileMenu.add(recentFile);
+        //recentFile.add(recent);
+
         editMenu.add(copy);
         editMenu.add(paste);
-        newFile.addActionListener(this);
-        newFile.setActionCommand("new");
-        saveFile.addActionListener(this);
-        saveFile.setActionCommand("save");
-        printFile.addActionListener(this);
-        printFile.setActionCommand("print");
-        copy.addActionListener(this);
-        copy.setActionCommand("copy");
-        paste.addActionListener(this);
-        paste.setActionCommand("paste");
-        undo.addActionListener(this);
-        undo.setActionCommand("undo");
+        editMenu.add(replace);
+
+        newFile.addActionListener(this::actionNew);
+        //actionNew.setActionCommand("new");
+        saveFile.addActionListener(this::actionSave);
+        //actionSave.setActionCommand("save");
+        printFile.addActionListener(this::actionPrint);
+        //actionPrint.setActionCommand("print");
+        copy.addActionListener(this::actionCopy);
+        //actionCopy.setActionCommand("copy");
+        paste.addActionListener(this::actionPaste);
+        //actionPaste.setActionCommand("paste");
+        openFile.addActionListener(this::actionOpen);
+        replace.addActionListener(this::actionReplace);
+
         menu.add(fileMenu);
         menu.add(editMenu);
         setJMenuBar(menu);
@@ -73,7 +76,85 @@ public class SimpleNotePad extends JFrame implements ActionListener{
         SimpleNotePad app = new SimpleNotePad();
     }
 
-    @Override
+    private void actionOpen(ActionEvent e){
+        File fileToOpen = null;
+        JFileChooser file = new JFileChooser();
+        int result = file.showOpenDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION)
+            fileToOpen = file.getSelectedFile();
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(fileToOpen)));
+            text.read(input, "Opening...");
+            input.close();
+        } catch (IOException ex) {
+        }
+    }
+
+    private void actionReplace(ActionEvent e){
+        JOptionPane replaceText = new JOptionPane();
+        text.replaceSelection(replaceText.showInputDialog("Enter or paste text to insert: "));
+    }
+
+    private void actionRecent(){
+
+    }
+
+    private void actionNew(ActionEvent e){
+        text.setText("");
+    }
+
+    private void actionPaste(ActionEvent e) {
+        StyledDocument doc = text.getStyledDocument();
+        Position position = doc.getEndPosition();
+        System.out.println("offset"+position.getOffset());
+        text.paste();
+    }
+
+    private void actionCopy(ActionEvent e) {
+        text.copy();
+    }
+
+    private void actionPrint(ActionEvent e) {
+        try{
+            PrinterJob pjob = PrinterJob.getPrinterJob();
+            pjob.setJobName("Sample Command Pattern");
+            pjob.setCopies(1);
+            pjob.setPrintable(new Printable() {
+                public int print(Graphics pg, PageFormat pf, int pageNum) {
+                    if (pageNum>0)
+                        return Printable.NO_SUCH_PAGE;
+                    pg.drawString(text.getText(), 500, 500);
+                    paint(pg);
+                    return Printable.PAGE_EXISTS;
+                }
+            });
+
+            if (pjob.printDialog() == false)
+                return;
+            pjob.print();
+        } catch (PrinterException pe) {
+            JOptionPane.showMessageDialog(null,
+                    "Printer error" + pe, "Printing error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void actionSave(ActionEvent e) {
+        File fileToWrite = null;
+        JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showSaveDialog(null);
+        if (returnVal == JFileChooser.APPROVE_OPTION)
+            fileToWrite = fc.getSelectedFile();
+        try {
+            PrintWriter out = new PrintWriter(new FileWriter(fileToWrite));
+            out.println(text.getText());
+            JOptionPane.showMessageDialog(null, "File is saved successfully...");
+            out.close();
+        } catch (IOException ex) {
+        }
+    }
+
+    /*@Override
     public void actionPerformed(ActionEvent e) {
         if(e.getActionCommand().equals("new")) {
             text.setText("");
@@ -120,8 +201,6 @@ public class SimpleNotePad extends JFrame implements ActionListener{
             Position position = doc.getEndPosition();
             System.out.println("offset"+position.getOffset());
             text.paste();
-        }else if(e.getActionCommand().equals("undo")) {
-//TODO: implement undo operation
         }
-    }
+    }*/
 }
