6274a461fb548a36e6a3051b180190a8d25ad878
Nick Brouwer <44852318+nebrouwer@users.noreply.github.com>
2019-06-11 20:19:53 -0700

    Final commit edit - rearranged code into three different classes

--- src/SimpleNotePad/Actions.java
+++ src/SimpleNotePad/Actions.java
@@ -2,6 +2,8 @@
 // CS 410
 // Assignment 3
 
+package SimpleNotePad;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,63 +14,24 @@ import java.awt.print.PrinterJob;
 import java.io.*;
 import java.util.LinkedList;
 
-public class SimpleNotePad extends JFrame{
-    JMenuBar menu = new JMenuBar();
-    JMenu fileMenu = new JMenu("File");
-    JMenu editMenu = new JMenu("Edit");
+public class Actions {
     JTextPane text = new JTextPane();
-    JMenuItem newFile = new JMenuItem("New File");
-    JMenuItem saveFile = new JMenuItem("Save File");
-    JMenuItem printFile = new JMenuItem("Print File");
-    JMenuItem openFile = new JMenuItem("Open File");
     JMenu recentFile = new JMenu("Recent");
-    JMenuItem replace = new JMenuItem("Replace");
-    JMenuItem copy = new JMenuItem("Copy");
-    JMenuItem paste = new JMenuItem("Paste");
-    LinkedList<File> recents = new LinkedList<>();
+    private LinkedList<File> recents = new LinkedList<>();
 
-    public SimpleNotePad()
-    {
-        setTitle("A Simple Notepad Tool");
-        fileMenu.add(newFile);
-        fileMenu.add(openFile);
-        fileMenu.add(saveFile);
-        fileMenu.add(printFile);
-        fileMenu.add(recentFile);
+    public Actions(){}
 
-        editMenu.add(copy);
-        editMenu.add(paste);
-        editMenu.add(replace);
-
-        newFile.addActionListener(this::actionNew);
-        saveFile.addActionListener(this::actionSave);
-        printFile.addActionListener(this::actionPrint);
-        copy.addActionListener(this::actionCopy);
-        paste.addActionListener(this::actionPaste);
-        openFile.addActionListener(this::actionOpen);
-        replace.addActionListener(this::actionReplace);
-
-        menu.add(fileMenu);
-        menu.add(editMenu);
-        setJMenuBar(menu);
-        add(new JScrollPane(text));
-        setPreferredSize(new Dimension(600,600));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        pack();
-    }
-
-    public static void main(String[] args) {
-        SimpleNotePad app = new SimpleNotePad();
+    public JTextPane getTextPane(){
+        return text;
     }
 
     // ActionEvent method for New File function
-    private void actionNew(ActionEvent e){
+    public void actionNew(ActionEvent e){
         text.setText("");
     }
 
     // ActionEvent method for Open File function
-    private void actionOpen(ActionEvent e){
+    public void actionOpen(ActionEvent e){
         File fileToOpen = null;
         JFileChooser file = new JFileChooser();
         int result = file.showOpenDialog(null);
@@ -90,7 +53,7 @@ public class SimpleNotePad extends JFrame{
     }
 
     // ActionEvent method for Save File function
-    private void actionSave(ActionEvent e) {
+    public void actionSave(ActionEvent e) {
         File fileToWrite = null;
         JFileChooser fc = new JFileChooser();
         int returnVal = fc.showSaveDialog(null);
@@ -110,7 +73,7 @@ public class SimpleNotePad extends JFrame{
     }
 
     // ActionEvent method for Print File function
-    private void actionPrint(ActionEvent e) {
+    public void actionPrint(ActionEvent e) {
         try{
             PrinterJob pjob = PrinterJob.getPrinterJob();
             pjob.setJobName("Sample Command Pattern");
@@ -120,7 +83,7 @@ public class SimpleNotePad extends JFrame{
                     if (pageNum>0)
                         return Printable.NO_SUCH_PAGE;
                     pg.drawString(text.getText(), 500, 500);
-                    paint(pg);
+                    text.paint(pg);
                     return Printable.PAGE_EXISTS;
                 }
             });
@@ -156,22 +119,22 @@ public class SimpleNotePad extends JFrame{
     }
 
     // ActionEvent method for Recent function
-    private void actionRecent(ActionEvent e){
+    public void actionRecent(ActionEvent e){
         open(new File(e.getActionCommand()));
     }
 
     // ActionEvent method for Copy function
-    private void actionCopy(ActionEvent e) {
+    public void actionCopy(ActionEvent e) {
         text.copy();
     }
 
     // ActionEvent method for Paste function
-    private void actionPaste(ActionEvent e) {
+    public void actionPaste(ActionEvent e) {
         text.paste();
     }
 
     // ActionEvent method for Replace function
-    private void actionReplace(ActionEvent e){
+    public void actionReplace(ActionEvent e){
         String userInput = JOptionPane.showInputDialog("Enter or paste text to replace selection:");
         if(userInput != null) {
             text.replaceSelection(userInput);
